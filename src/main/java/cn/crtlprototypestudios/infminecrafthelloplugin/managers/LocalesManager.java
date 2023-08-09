2857d00116a18504829c71a1e681e9e560da1d3a
Type-32 <87076491+Type-32@users.noreply.github.com>
2023-08-09 12:51:53 +0800

    Added UTF-8 Encoding Processor for Locale Bundles

--- src/main/java/cn/crtlprototypestudios/infminecrafthelloplugin/managers/LocalesManager.java
+++ src/main/java/cn/crtlprototypestudios/infminecrafthelloplugin/managers/LocalesManager.java
@@ -1,5 +1,6 @@
 package cn.crtlprototypestudios.infminecrafthelloplugin.managers;
 
+import cn.crtlprototypestudios.infminecrafthelloplugin.UTF8Control;
 import org.bukkit.entity.Player;
 
 import java.util.Locale;
@@ -8,9 +9,9 @@ import java.util.ResourceBundle;
 public class LocalesManager {
     public static ResourceBundle Locales = ResourceBundle.getBundle("messages");
     public static void fetchLocale(String locale){
-        Locales = ResourceBundle.getBundle("messages", new Locale(locale));
+        Locales = ResourceBundle.getBundle("messages", new Locale(locale), new UTF8Control());
     }
     public static void fetchPlayerLocale(Player player){
-        Locales = ResourceBundle.getBundle("messages", Locale.forLanguageTag(player.getLocale()));
+        Locales = ResourceBundle.getBundle("messages", Locale.forLanguageTag(player.getLocale()), new UTF8Control());
     }
 }
