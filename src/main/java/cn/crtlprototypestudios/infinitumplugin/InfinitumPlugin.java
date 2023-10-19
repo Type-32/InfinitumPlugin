aca490af1d193f89c16086738116c8a044b532d3
Type-32 <87076491+Type-32@users.noreply.github.com>
2023-10-19 22:56:18 +0800

    Changed Some Stuff I Don't Remember

--- src/main/java/cn/crtlprototypestudios/infinitumplugin/InfinitumPlugin.java
+++ src/main/java/cn/crtlprototypestudios/infinitumplugin/InfinitumPlugin.java
@@ -10,6 +10,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public class InfinitumPlugin extends JavaPlugin {
 
     private static InfinitumPlugin instance;
@@ -23,24 +25,26 @@ public class InfinitumPlugin extends JavaPlugin {
 
         // Register your commands and event listeners here
         WaypointCommand waypointCommand = new WaypointCommand();
-        RulesCommand rulesCommand = new RulesCommand();
+//        RulesCommand rulesCommand = new RulesCommand();
         TpaCommand tpaCommand = new TpaCommand();
         FactionsCommand factionsCommand = new FactionsCommand();
 
-        getCommand("tpa").setExecutor(tpaCommand);
-        getCommand("tpaccept").setExecutor(tpaCommand);
-        getCommand("tpdeny").setExecutor(tpaCommand);
+        Objects.requireNonNull(getCommand("tpa")).setExecutor(tpaCommand);
+        Objects.requireNonNull(getCommand("tpaccept")).setExecutor(tpaCommand);
+        Objects.requireNonNull(getCommand("tpdeny")).setExecutor(tpaCommand);
 
-        getCommand("back").setExecutor(new BackCommand());
-        getCommand("spawn").setExecutor(new SpawnCommand());
+        Objects.requireNonNull(getCommand("back")).setExecutor(new BackCommand());
+        Objects.requireNonNull(getCommand("spawn")).setExecutor(new SpawnCommand());
 
-        getCommand("waypoint").setExecutor(waypointCommand);
-        getCommand("waypoint").setTabCompleter(waypointCommand);
+        Objects.requireNonNull(getCommand("waypoint")).setExecutor(waypointCommand);
+        Objects.requireNonNull(getCommand("waypoint")).setTabCompleter(waypointCommand);
 
-        getCommand("factions").setExecutor(factionsCommand);
-        getCommand("factions").setTabCompleter(factionsCommand);
-        //getCommand("rules").setExecutor(rulesCommand);
-        //getCommand("rules").setTabCompleter(rulesCommand);
+        Objects.requireNonNull(getCommand("factions")).setExecutor(factionsCommand);
+        Objects.requireNonNull(getCommand("factions")).setTabCompleter(factionsCommand);
+        /*
+        getCommand("rules").setExecutor(rulesCommand);
+        getCommand("rules").setTabCompleter(rulesCommand);
+        */
 
         WaypointManager.readAllWaypoints(this);
 
@@ -49,16 +53,12 @@ public class InfinitumPlugin extends JavaPlugin {
     }
 
     @Override
-    public void onDisable(){
+    public void onDisable() {
         WaypointManager.saveAllWaypoints(this);
     }
 
     public static InfinitumPlugin getInstance() {
         return instance;
-    }
-
-    public static Location getWorldSpawnLocation(World world) {
-        return world.getSpawnLocation();
     }
 
     @Override
